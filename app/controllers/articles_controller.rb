efd6546476090124012535378f7233498739094b
Philip <philipmjobe@gmail.com>
2021-10-29 13:24:39 -0400

    added correct web request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,4 @@
 class ArticlesController < ApplicationController
+  response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&from=2021-10-29&sortBy=popularity&apiKey=")
+  render json: response.parse(:json)
 end
