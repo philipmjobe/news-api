325da2aa5781f00141044a35bb71ac8d272e2932
Philip <philipmjobe@gmail.com>
2021-10-29 15:14:04 -0400

    messed with the api key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
 class ArticlesController < ApplicationController
-  response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&from=2021-10-29&sortBy=popularity&apiKey=")
+  response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&from=2021-10-29&sortBy=popularity&apiKey=e")
   render json: response.parse(:json)
 end
