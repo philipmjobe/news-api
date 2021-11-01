58f62956cc96d440fc0467907bfb7d8950e5421f
Philip <philipmjobe@gmail.com>
2021-11-01 14:36:02 -0400

    added the credentials stuff i think

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&from=2021-10-29&sortBy=popularity&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
     render json: response.parse(:json)
   end 
 end
